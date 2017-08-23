1963bb1d337cc0d55a7124b82013b99c249cb145
Xcode <xcode@goomailnoMacBook-Pro.local>
2017-08-23 14:58:46 +0900

    test

--- SimpleCount/ViewController.swift
+++ SimpleCount/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number = 0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +22,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+    }
 
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+    }
+
+    @IBAction func clear(){
+        number = 0
+        label.text = String(number)
+    }
+    
+    @IBAction func double(){
+        number = number * 2
+        label.text = String(number)
+    }
 
 }
 
